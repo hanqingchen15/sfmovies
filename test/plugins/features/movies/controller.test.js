222c8638bb83de6c5f41d277c76f72833d69e061
Hanqing Chen <h.chen93@gmail.com>
2019-11-14 13:11:39 -0800

    feat(add_get_routes): add GET endpoints for movies

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -1,17 +1,49 @@
 'use strict';
 
+const Knex = require('../../../../lib/libraries/knex');
+
 const Controller = require('../../../../lib/plugins/features/movies/controller');
 
 describe('movie controller', () => {
 
+  const payload = { name: 'test', release_year: 2000 };
+
+  beforeEach(async () => {
+    await Knex.truncate('movies');
+  });
+
   describe('create', () => {
 
     it('creates a movie', async () => {
-      const payload = { name: 'foo' };
-
       const movie = await Controller.create(payload);
-
       expect(movie.get('name')).to.eql(payload.name);
+    });
+
+  });
+
+  describe('fetch', () => {
+
+    let movie = {};
+    beforeEach(async () => {
+      movie = await Controller.create(payload);
+    });
+
+    it('fetches a movie', async () => {
+      const fetchedMovie = await Controller.fetch(movie.id);
+      expect(fetchedMovie.get('name')).to.eql(payload.name);
+    });
+
+  });
+
+  describe('fetch all', () => {
+
+    beforeEach(async () => {
+      await Controller.create(payload);
+    });
+
+    it('fetches all movies', async () => {
+      const movies = await Controller.fetchAll();
+      expect(movies.length).to.eql(1);
     });
 
   });
